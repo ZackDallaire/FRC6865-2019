267bf0a707330f38fe20d8626d7a4d0b69b4a3a0
Dalla <Dalla@192.168.2.8>
2019-12-18 13:19:25 -0500

    The Robot Code so far
    - Added Packages and Comments onto it.
    
    Not yet included the drivables but will be talked about at tommorows
    meeting

---
+++ Robotics 6865/src/Robot.java
@@ -0,0 +1,23 @@
+/* Project done by Manitoulin Metal team 6865
+*/
+
+package org.usfirst.frc.team6865.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpu.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DiffrentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/** 
+
+* This Program has some parts for the 2018 Program as it was used a basis but not the whole thing.abstract
+
+* If you have any problems speak to Zachary Dallaire
+*/
