cf56f3bf0da345eaea3e2aea918f9a668bd8fb0d
ZackDallaire <58746282+ZackDallaire@users.noreply.github.com>
2019-12-20 19:11:01 -0500

    Fixed spelling errors
    Fixed the spelling errors on the import tufff

--- Robotics 6865/src/Robot.java
+++ Robotics 6865/src/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpu.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
