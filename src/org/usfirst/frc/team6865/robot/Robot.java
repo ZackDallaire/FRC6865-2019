0f556d3ffc47b2aab4cb6e5fe5ed1a7327797d17
Dalla <Dalla@10.231.202.97>
2020-01-09 11:12:43 -0500

    Put in the ability to drive .....

--- src/org/usfirst/frc/team6865/robot/Robot.java
+++ src/org/usfirst/frc/team6865/robot/Robot.java
@@ -11,17 +11,42 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.cameraserver.CameraServer;
+
 
 public class Robot extends TimedRobot {
+public Spark feed = new Spark(2);
+public Spark arm = new Spark(6);
+
+// Set Joysticks 
+
+private Joystick bigJ = new Joystick(1);
+private Joystick xBox = new Joystick(0);
+
+
+// Set Constants
+
+private final double deadZone = 0.05;
 
     @Override
     public void robotInit() {
         // This is called once when the robot code initializes
+    	SmartDashboard.putNumber("DrivePower",0.82);
+    	
+    	// Camera USB
+    	CameraServer.getinstance().startAutomaticCapture();
+    	
     }
 
     @Override
     public void robotPeriodic() {
         // This is called every period regardless of mode
+    	
+    	
     }
 
     @Override
@@ -37,6 +62,7 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopInit() {
         // This is called once when the robot first enters teleoperated mode
+    	
     }
 
     @Override
