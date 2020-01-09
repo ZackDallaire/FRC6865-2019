e09095914c677c7e401d1a535134c0c06e606e46
Dalla <Dalla@10.231.202.97>
2020-01-09 11:18:38 -0500

    I lied I belive this is what we need to drive

--- src/org/usfirst/frc/team6865/robot/Robot.java
+++ src/org/usfirst/frc/team6865/robot/Robot.java
@@ -62,12 +62,25 @@ private final double deadZone = 0.05;
     @Override
     public void teleopInit() {
         // This is called once when the robot first enters teleoperated mode
-    	
+    	d
     }
 
     @Override
     public void teleopPeriodic() {
         // This is called periodically while the robot is in teleopreated mode
+    	
+    	double drivePower = SmartDashboard.getNumber('DrivePower',0.7);
+    	
+    	// Insert Saftys once we know what were doing
+    	
+    	if(Math.abs(bigJ.getY()) > deadZone || Math.abs(bigJ.getX()) > deadZone) {
+    		move.arcadeDrive(bigJ.getY()*drivePower,bigJ.getX()*drivePower);
+    	}
+    	else {
+    		move.arcadeDrive(0,0,);
+    	}// End of the drive base.
+    	
+    	
     }
 
     @Override
