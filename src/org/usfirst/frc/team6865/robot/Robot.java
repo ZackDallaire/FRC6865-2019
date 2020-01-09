18362cb78437795b09343977336a42626b1d09c6
Dalla <Dalla@10.231.202.97>
2020-01-09 10:50:07 -0500

    Added classes so that way its eaisser to rewrite it

--- src/org/usfirst/frc/team6865/robot/Robot.java
+++ src/org/usfirst/frc/team6865/robot/Robot.java
@@ -10,5 +10,48 @@ package org.usfirst.frc.team6865.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.TimedRobot;
 
-/**
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+        // This is called once when the robot code initializes
+    }
+
+    @Override
+    public void robotPeriodic() {
+        // This is called every period regardless of mode
+    }
+
+    @Override
+    public void autonomousInit() {
+        // This is called once when the robot first enters autonomous mode
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        // This is called periodically while the robot is in autonomous mode
+    }
+
+    @Override
+    public void teleopInit() {
+        // This is called once when the robot first enters teleoperated mode
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        // This is called periodically while the robot is in teleopreated mode
+    }
+
+    @Override
+    public void testInit() {
+        // This is called once when the robot enters test mode
+    }
+
+    @Override
+    public void testPeriodic() {
+        // This is called periodically while the robot is in test mode
+    }
+
+}
